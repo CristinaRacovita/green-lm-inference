bb782a8d5e0e820064129524f4c0f96ba4b61821
Cristina Racovita <cristina.racovita98@gmail.com>
2024-10-18 21:54:37 +0200

    implement get k most similar items for weaviate db

--- weaviate_index.py
+++ weaviate_index.py
@@ -1,6 +1,7 @@
 import weaviate
-from weaviate.classes.config import Property, DataType
-from weaviate.classes.query import Filter
+from weaviate.classes.config import Property, DataType, Configure
+from weaviate.classes.query import Filter, MetadataQuery
+from weaviate.classes.data import DataObject
 from read_files import read_from_directory
 
 # If not specified explicitly, the default distance metric in Weaviate is cosine
@@ -16,23 +17,48 @@ def create_collections(collection_names, weaviate_client):
                 collection_name,
                 properties=[
                     Property(name="text", data_type=DataType.TEXT),
-                    Property(name="embedding", data_type=DataType.NUMBER_ARRAY),
                 ],
+                vectorizer_config=Configure.Vectorizer.none(),
             )
 
 
 def add_objects_for_collection(collection_name, weaviate_client, data):
     collection = weaviate_client.collections.get(collection_name)
     collection.data.delete_many(where=Filter.by_property("text").like("*"))
-    for element in data:
-        collection.data.insert(
-            {"text": element["text"], "embedding": element["embedding"]}
-        )
+    data_objs = []
+    for d in data:
+        data_objs.append(DataObject(
+            properties={
+                "text": d["text"],
+            },
+            vector=d["embedding"]
+        ))
+
+    collection = weaviate_client.collections.get(collection_name)
+    collection.data.insert_many(data_objs)
+    # with collection.batch.dynamic() as batch:
+    #     for element in data:
+    #         batch.add_object(
+    #             properties={"text": element["text"]},
+    #             vector=element["embedding"]
+    #         )
+    # if len(collection.batch.failed_objects) > 0:
+    #     print(f"Failed to import {len(collection.batch.failed_objects)} objects")
 
 def get_all_data_from_collection(collection_name, client):
     collection = client.collections.get(collection_name)
     for item in collection.iterator():
-        print(item.uuid, item.properties)
+        print(item.uuid, item.properties, item.vector)
+
+def get_k_most_similar(weaviate_client, collection_name, embedding, k):
+    collection = weaviate_client.collections.get(collection_name)
+    response = collection.query.near_vector(
+        near_vector=embedding,
+        limit=k,
+        return_metadata=MetadataQuery(certainty=True)
+    )
+
+    print(response)
 
 if __name__ == "__main__":
     client = weaviate_connect()
@@ -44,7 +70,19 @@ if __name__ == "__main__":
             create_collections(collection_names, client)
             for index, collection_name in enumerate(collection_names):
                 add_objects_for_collection(collection_name, client, json_data[index])
-                get_all_data_from_collection(collection_name, client)
+                # get_all_data_from_collection(collection_name, client)
+                get_k_most_similar(client, collection_name, [
+                        0.12,
+                        0.87,
+                        -0.44,
+                        0.66,
+                        -0.01,
+                        0.23,
+                        0.99,
+                        -0.78,
+                        0.11,
+                        0.34
+                    ], 1)
         except Exception as error:
             print(error)
         finally:
