5aa677009735bf4cf96d7d8440d82627d76a6b22
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-10-23 20:44:14 +0200

    start implementing RAG

---
+++ retrieval_augmented_generation/run_rag.py
@@ -0,0 +1,8 @@
+
+# load embedding model, language model, vector db
+
+# get queries
+
+# for each query: embed it, get from the vector db top k answers, create the prompt, generate answer
+
+# before benchmarking answer to one question
