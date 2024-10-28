c00e2910ea78b322158f2712480a791369747161
Bindila-Bogdan <b.mihaibogdan@yahoo.com>
2024-10-28 19:19:15 +0100

    increase recording time for the idle states

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,16 @@
+echo "wait for the consumption to normalize"
+sleep 30
+
 echo "recording idle measurements..."
-sleep 310
+sleep 510
 python ../utils.py "idle state"
 
 # start the Docker daemon
 "C:\Program Files\Docker\Docker\Docker Desktop.exe" 
-sleep 30
+sleep 60
 
 echo "recording measurements while Docker is running..."
-sleep 310
+sleep 510
 python ../utils.py "Docker running"
 
 # pull the language models
